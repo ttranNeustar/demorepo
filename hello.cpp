8cc103481f144b15919b9469f221a8f476890a79
s_skalsa <s_skalsa>
2015-03-09 06:06:39 -0400

    Adding test program in my demo git repo.

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// This is test hello world program in my git repo.
+//
+#include <iostream>
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+	cout<<endl<<"Hello, GIT HUB"<<endl;
+	return 0;
+}
